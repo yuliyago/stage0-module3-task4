79685147c51181ba498c0d058df3f4e8958cd110
yuliyago <yuuliya02@proton.me>
2023-06-21 18:49:47 +0300

    Work with primitives was done.

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char a='a',b='b';
+        System.out.println((9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2));
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c =99;
+        char h =104;
+        char a=97;
+        char r=114;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+//        System.out.println(c+""+h+""+a+""+r);
     }
 }
